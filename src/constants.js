64fd9f29fc86788725f2250dff3142475816ec12
Jiaxi Huang <huangjx1025@gmail.com>
2026-05-03 21:03:12 +0100

    Delete src directory

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-const ASSETS_BASE = 'https://assets.jimmyhuang.cn'
-const BUILD_HASH = typeof __BUILD_HASH__ !== 'undefined' ? __BUILD_HASH__ : 'dev-' + Math.random().toString(36).slice(2, 14)
-export { BUILD_HASH }
-export default ASSETS_BASE
